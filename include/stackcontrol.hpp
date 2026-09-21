c1ffa69d33b5e7b4aa98ddf1d32b2d9063129e4b
ninjasnowball <ninjasnowball23@gmail.com>
2026-09-21 15:31:41 -0500

    Adding return to start functionality

--- include/stackcontrol.hpp
+++ include/stackcontrol.hpp
@@ -23,6 +23,8 @@ class StackController{
 
         Position nextPosition();
 
+        Position returnToStart();
+
         int CloseStack();
 
     private:
@@ -53,11 +55,73 @@ Position nextPosition(){
     auto downIt = std::find(downVec.begin(), downVec.end(), this->state);
     if(upDaigIt != upDiagVec.end()){
         //Move up and to the right one box
+        stackMoveDiagonal(this->stackPort, 3.0, 0.5, 0, 6.0, 1.0, 0);
     } else if (downIt != downVec.end()){
         //Move down one box
+        stackMove(this->stackPort, 3.0, 1.0, 1, 0);
     } else if (state == BugThree){
         //Move down one and left two boxes
+        stackMoveDiagonal(this->stackPort, 3.0, 0.25, 1, 12.0, 1.0, 1);
     } else if (state == BugSix){
         //Move up three and left two boxes
+        stackMoveDiagonal(this->stackPort, 9.0, 0.75, 0, 12.0, 1.0, 1);
     }
+
+    this->state += 1;
+    return this->state;
+}
+
+Position returnToStart(){
+
+    switch(this->state){
+        case: CardOne
+            break;
+        case: BugOne
+            //Move up one
+            stackMove(this->stackPort, 3.0, 1.0, 0, 0);
+            break;
+        case: CardTwo
+            //Move left one
+            stackMove(this->stackPort, 6.0, 1.0, 1, 1);
+            break;
+        case: BugTwo
+            //Move up one and left one
+            stackMoveDiagonal(this->stackPort, 3.0, 1.0, 0, 6.0, 1.0, 1);
+            break;
+        case: CardThree
+            //Move left two
+            stackMove(this->stackPort, 12.0, 1.0, 1, 1);
+            break;
+        case: BugThree
+            //Move up one and left two
+            stackMoveDiagonal(this->stackPort, 3.0, 1.0, 0, 12.0, 1.0, 1);
+            break;
+        case CardFour
+            //Move up two
+            stackMove(this->stackPort, 6.0, 1.0, 0, 0);
+            break;
+        case BugFour
+            //Move up three
+            stackMove(this->stackPort, 9.0, 1.0, 0, 0);
+            break;
+        case CardFive
+            //Move up two and left one
+            stackMoveDiagonal(this->stackPort, 6.0, 1.0, 0, 6.0, 1.0, 1);
+            break;
+        case BugFive
+            //Move up three and left one
+            stackMoveDiagonal(this->stackPort, 9.0, 1.0, 0, 6.0, 1.0, 1);
+            break;
+        case CardSix
+            //Move up two and left two
+            stackMoveDiagonal(this->stackPort, 6.0, 1.0, 0, 12.0, 1.0, 1);
+            break;
+        case BugSix
+            //Move up three and left two
+            stackMoveDiagonal(this->stackPort, 9.0, 1.0, 0, 12.0, 1.0, 1);
+            break;
+    }
+
+    this->state = CardOne;
+    return this->state;
 }
